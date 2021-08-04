b21fbe538619e67e98fbeb87b0d22166bc24ca98
Jorge Luís <47546263+jorgelgf@users.noreply.github.com>
2021-08-04 20:53:27 -0300

    Add files via upload

---
+++ exercising/list/14.js
@@ -0,0 +1,9 @@
+
+const arrayOfArray = ar =>{
+//convertendo objeto para dentro de array
+console.log(Object.entries(ar))
+
+  return 0;
+}
+
+console.log(arrayOfArray({  nome: "Maria",  profissao: "Desenvolvedora de software"}))
